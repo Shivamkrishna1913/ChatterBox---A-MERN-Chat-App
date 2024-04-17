9ddfff8ab256c5b51db90c0825bed9de81ced70b
Shivam maurya <shivamaurya191002@gmail.com>
2024-04-17 21:39:45 +0530

    logout functionality done

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { TbLogout2 } from "react-icons/tb";
+import useLogout from "../../hooks/useLogout.jsx";
+
 const LogoutButton = () => {
+  const { loading, logout } = useLogout();
+
   return (
     <>
       <div className="mt-auto ">
-        <button>
-          <TbLogout2 className="h-8 w-8 hover:text-white " />
-        </button>
+        {!loading ? (
+          <TbLogout2
+            className="h-8 w-8 hover:text-white cursor-pointer"
+            onClick={logout}
+          />
+        ) : (
+          <span className="loading loading-spinner"></span>
+        )}
       </div>
     </>
   );
