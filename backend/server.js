7983081ce68a3b7651ac8686184f87672fcb0708
Shivam maurya <shivamaurya191002@gmail.com>
2024-04-30 15:13:35 +0530

    c

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -6,8 +7,11 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+// const app = express();
+import { app, server } from "./socket/socket.js";
+
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -19,7 +23,13 @@ app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 // app.get("/api/auth",authRoutes)
 
-app.listen(PORT, () => {
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
